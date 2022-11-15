b198a736d95603105c4481880cb412b8da3c41cd
Saša Barišić <sbarisic@vub.hr>
2022-11-15 21:30:52 +0100

    Changes

--- SPA_1/program.c
+++ SPA_1/program.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DQUEUE_MAXLENGTH 16
+#define DQUEUE_MAXLENGTH 10
 
 typedef struct {
 	int elements[DQUEUE_MAXLENGTH];
@@ -9,64 +9,93 @@ typedef struct {
 	int last;
 } DQueue;
 
+// Izjednaciti first i last da dqueue bude "prazan"
 void MakeNull(DQueue* q) {
 	q->first = 0;
 	q->last = 0;
-}
-
-void EnqueueFront(DQueue* q, int element) {
-	q->elements[q->first] = element;
-	q->first++;
-
-	if (q->first >= DQUEUE_MAXLENGTH)
-		q->first = 0;
-}
-
-void EnqueueBack(DQueue* q, int element) {
-	q->last--;
-
-	if (q->last < 0)
-		q->last = DQUEUE_MAXLENGTH - 1;
-
-	q->elements[q->last] = element;
-}
-
-int DequeueFront(DQueue* q) {
-	q->first--;
-
-	if (q->first < 0)
-		q->first = DQUEUE_MAXLENGTH - 1;
-
-	int index = q->first;
-
-	int result = q->elements[index];
-	q->elements[index] = -1;
-	return result;
-}
-
-int DequeueBack(DQueue* q) {
-	int index = q->last;
-	q->last++;
-
-	if (q->last >= DQUEUE_MAXLENGTH)
-		q->last = 0;
-
-	int result = q->elements[index];
-	q->elements[index] = -1;
-	return result;
 }
 
 int Empty(DQueue* q) {
 	return q->first == q->last;
 }
 
+// U obicnom queue-u je ovo funkcija Enqueue
+void EnqueueFront(DQueue* q, int element) {
+	// Postaviti element u prvo slobodno mjesto sa prednje strane (first po defaultu pokazuje na prvo slobodno mjesto)
+	q->elements[q->first] = element;
+
+	// Povecati indeks na prvo slobodno mjesto
+	q->first++;
+
+	// Wrap around
+	// Ako je indeks veci od duzine polja, idemo ispocetka
+	// Jer MakeNull ne mora nuzno postaviti i first i last u nulu
+	// To moze biti bilo koji arbitrarni indeks u polju
+	if (q->first >= DQUEUE_MAXLENGTH)
+		q->first = 0;
+}
+
+void EnqueueBack(DQueue* q, int element) {
+	// Smanjiti indeks za -1 da pokazuje na prvo slobodno mjesto odozada (last po defaultu pokazuje na prvo zauzeto mjesto)
+	q->last--;
+
+	// Wrap around
+	if (q->last < 0)
+		q->last = DQUEUE_MAXLENGTH - 1;
+
+	// Postaviti element u prvo slobodno mjesto sa zadnje strane
+	q->elements[q->last] = element;
+}
+
+int DequeueFront(DQueue* q) {
+	// Smanjiti indeks za -1 da pokazuje na prvo zauzeto mjesto sa prednje strane
+	q->first--;
+
+	// Wrap around
+	if (q->first < 0)
+		q->first = DQUEUE_MAXLENGTH - 1;
+
+	int index = q->first;
+
+	// Dohvati element i makni iz polja
+	int result = q->elements[index];
+	q->elements[index] = -1; // Ovo je realno nepotrebno
+
+	return result;
+}
+
+// U obicnom queue-u je ovo funkcija Dequeue
+int DequeueBack(DQueue* q) {
+	// Spremiti last u indeks jer vec pokazuje na prvo zauzeto mjesto sa zadnje strane
+	int index = q->last;
+
+	// Povecati da pokazuje na drugo zauzeto mjesto sa zadnje strane jer ovo prvo micemo van
+	q->last++;
+
+	// Wrap around
+	if (q->last >= DQUEUE_MAXLENGTH)
+		q->last = 0;
+
+	// Dohvati element i makni iz polja
+	int result = q->elements[index];
+	q->elements[index] = -1;
+
+	return result;
+}
+
 //-------------------------------------------------------------------------------
 
 
 void PrintQueue(DQueue* q) {
+	// Ovdje koristimo jos jedan queue da dohvatimo sve elemente iz prvog i spremimo u drugi
+	// Razlog tome je jer ne mozemo indeksirati bilo koji element u queueu koji nije na pocetku i na kraju,
+	// a moramo ispisati sve elemente u queueu
+
 	DQueue temp_queue;
 	MakeNull(&temp_queue);
 
+	// Dok ima elemenata u queueu, dohvati sa pocetka, ispisi na ekran i gurni u drugi queue na pocetak
+	// Ta operacija okrece redoslijed kao u stacku, znaci iz queuea 3 2 1 -> u queue 1 2 3 (jer uzimamo sa kraja i stavljamo na kraj)
 	while (!Empty(q)) {
 		int num = DequeueFront(q);
 		printf("%d ", num);
@@ -74,6 +103,7 @@ void PrintQueue(DQueue* q) {
 		EnqueueFront(&temp_queue, num);
 	}
 
+	// Vrati sve elemente iz drugog queuea u prvi na isti nacin
 	while (!Empty(&temp_queue)) {
 		int num = DequeueFront(&temp_queue);
 		EnqueueFront(q, num);
@@ -107,7 +137,7 @@ void DoData(DQueue* Q) {
 	EnqueueFront(Q, 17);
 	DequeueBack(Q);
 	DequeueFront(Q);
-	EnqueueFront(Q, 17);
+	EnqueueFront(Q, 69);
 	EnqueueFront(Q, 18);
 	EnqueueFront(Q, 19);
 	EnqueueFront(Q, 20);
@@ -115,14 +145,14 @@ void DoData(DQueue* Q) {
 	DequeueBack(Q);
 	DequeueBack(Q);
 	DequeueFront(Q);
-	EnqueueBack(Q, 3);
+	EnqueueBack(Q, 420);
 	EnqueueBack(Q, 4);
 	EnqueueBack(Q, 5);
 }
 
 void main() {
 	DQueue Q;
-	MakeNull(&Q);
+	MakeNull(&Q); // MakeNull u ovom slucaju postavlja oba indexa u 4
 
 	/*DoData(&Q);
 	while (1) {
